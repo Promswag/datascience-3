64002d254675adc403a726c702b7fdc0a84aca57
Guillaume Baumgartner <promswag@gmail.com>
2024-11-28 17:52:53 +0100

    fix

--- ex05/split.py
+++ ex05/split.py
@@ -1,18 +1,18 @@
 import pandas as pd
-import os
+import sys
 
-def main():
+def main(str):
     try:
-        path = os.path.join("..", "ressources", "Train_knight.csv")
-        df = pd.read_csv(path)
-        print(len(df))
+        df = pd.read_csv(str)
         df_validation = df.sample(frac=0.3)
         df.drop(df_validation.index, inplace=True)
         df_validation.to_csv("Validation_knight.csv", index=False)
         df.to_csv("Training_knight.csv", index=False)
-
     except Exception as error:
         print(f'{type(error).__name__} {error}')
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Please provide a file")
+        sys.exit(1)
+    main(sys.argv[1])
